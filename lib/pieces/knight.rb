bc5e33c72a4339f290b6f4229bfd8e62176f168d
katie <katiepb525@gmail.com>
2023-01-27 15:38:46 -0600

    create knight class that generates list of legal moves from given point

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,3 +1,30 @@
-class Knight
+# frozen_string_literal: true
 
+require './lib/place'
+
+# knight piece
+# - moves any direction in an L shape
+# - can 'hop' over other pieces
+class Knight
+  POSSIBLE_DIRECTIONS = [[2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1]].freeze
+
+  # generate list of legal moves from a single place
+  def legal_moves(place)
+    list = []
+    POSSIBLE_DIRECTIONS.each do |curr_direction|
+      result = []
+      result.push((place.x + curr_direction[0]))
+      result.push((place.y + curr_direction[1]))
+
+      next if result[0] >= 8 || result[1] >= 8 || result[0].negative? || result[1].negative?
+
+      new_place = Place.new(result[0], result[1])
+      list.push(new_place)
+    end
+    list
+  end
 end
+
+# place = Place.new(0,1)
+# knight = Knight.new
+# p knight.legal_moves(place)
