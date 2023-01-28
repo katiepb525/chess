6b29a6bde2d1245ccf748bcb376f2f83d1abf28f
katie <katiepb525@gmail.com>
2023-01-28 11:46:33 -0600

    write first test for knight (there must be a better way of doing this..)

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -1,2 +1,20 @@
+require './lib/pieces/knight'
+
 describe Knight do
+  describe '#legal_moves' do
+    context 'place is [0,1]' do
+      subject(:knight) { described_class.new }
+      let(:place) { double('Place', x_coord: 0, y_coord: 1)}
+
+      it 'returns correct list of coordinates' do
+        result = knight.legal_moves(place)
+        expect(result[0].x_coord).to eq(2)
+        expect(result[0].y_coord).to eq(2)
+        expect(result[1].x_coord).to eq(1)
+        expect(result[1].y_coord).to eq(3)
+        expect(result[2].x_coord).to eq(2)
+        expect(result[2].y_coord).to eq(0)
+      end
+    end
+  end
 end
