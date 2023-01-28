58b888e3b9461ed5442f6013e6c879a7e4debaa2
katie <katiepb525@gmail.com>
2023-01-28 11:53:21 -0600

    add light/dark unicode symbols for knight/rook

---
+++ lib/symbols.rb
@@ -0,0 +1,21 @@
+module Symbols
+  def rook_light
+    rook_light = "\u2656"
+    rook_light.encode('utf-8')
+  end
+
+  def rook_dark
+    rook_dark = "\u265c"
+    rook_dark.encode('utf-8')
+  end
+
+  def knight_light
+    knight_light = "\u2658"
+    knight_light.encode('utf-8')
+  end
+
+  def knight_dark
+    knight_dark = "\u265e"
+    knight_dark.encode('utf-8')
+  end
+end
