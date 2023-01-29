8e3c58b830a24e87f2756fd567825f6a2f988a94
katie <katiepb525@gmail.com>
2023-01-29 11:58:40 -0600

    write first test for rook.rb in rook_spec

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -1,2 +1,21 @@
+require './lib/pieces/rook'
+
 describe Rook do
+  describe '#legal_moves' do
+    subject(:rook) { described_class.new }
+
+    context 'place is [0,0]' do
+      let(:place) { double('Place', x_coord: 0, y_coord: 0) }
+      let(:result) { [] }
+
+      before do
+        result = rook.legal_moves(place)
+      end
+
+      it 'returns correct list of coordinates' do
+        expect(result).to all include([1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7])
+      end
+    end
+  end
+
 end
