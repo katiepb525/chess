2b999f1f7c86aca1f6abb1abcf7d6b1b1d53e567
katie <katiepb525@gmail.com>
2023-01-30 13:10:59 -0600

    create first test for rook#legal_moves

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -1,21 +1,26 @@
 require './lib/pieces/rook'
 
+require './lib/pieces/rook'
+
 describe Rook do
-  describe '#legal_moves' do
-    subject(:rook) { described_class.new }
+  subject(:rook) { described_class.new }
 
-    context 'place is [0,0]' do
-      let(:place) { double('Place', x_coord: 0, y_coord: 0) }
-      let(:result) { [] }
+  context 'place is [0,0]' do
+    let(:place) { double('Place', x_coord: 0, y_coord: 0) }
+    let(:result_arr) { [] }
 
-      before do
-        result = rook.legal_moves(place)
-      end
-
-      it 'returns correct list of coordinates' do
-        expect(result).to all include([1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7])
+    before do
+      result = rook.legal_moves(place)
+      result.each do |place|
+        arr = []
+        arr.push(place.x_coord, place.y_coord)
+        result_arr.push(arr)
       end
     end
+  
+    it 'returns correct list of coordinates with none missing' do
+      expect(result_arr).to include([1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7])
+      expect(result_arr.length).to be(14)
+    end
   end
-
 end
