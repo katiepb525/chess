9064d48529db3856b68901158789e272f21135a0
katie <katiepb525@gmail.com>
2023-01-30 18:28:21 -0600

    write second test for rook#legal_moves

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -24,4 +24,24 @@ describe Rook do
       expect(result_arr.length).to be(14)
     end
   end
+
+  context 'place is [7,7]' do
+    let(:place) { double('Place', x_coord: 7, y_coord: 7) }
+    let(:result_arr) { [] }
+
+    before do
+      result = rook.legal_moves(place)
+      result.each do |place|
+        arr = []
+        arr.push(place.x_coord, place.y_coord)
+        result_arr.push(arr)
+      end
+    end
+
+    it 'returns correct list of coordinates with none missing' do
+      expect(result_arr).to include([7, 6], [7, 5], [7, 4], [7, 3], [7, 2], [7, 1], [7, 0], [6, 7], [5, 7], [4, 7],
+                                    [3, 7], [2, 7], [1, 7], [0, 7])
+      expect(result_arr.length).to be(14)
+    end
+  end
 end
