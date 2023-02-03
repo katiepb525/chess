ef3eb5a9440ef7e51416b7cf015cd435f29bd1f5
katie <katiepb525@gmail.com>
2023-02-03 13:37:03 -0600

    store player_num (1 or 2), light or dark pieces, and raw input of player instances

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,23 @@
 # Stores information about a player instance.
 class Player
-  attr_reader :name, :light_or_dark
+  attr_reader :name, :player_num, :light_or_dark
+  attr_accessor :raw_input
 
-  def initialize(name, light_or_dark = :light)
+  def initialize(name, player_num)
     @name = name
-    @light_or_dark = light_or_dark
+    @player_num = player_num
+    @light_or_dark = light_or_dark_pieces?
+    @raw_input = nil
+  end
+
+  def light_or_dark_pieces?
+    case @player_num
+    when 1
+      :light
+    when 2
+      :dark
+    else
+      raise "Huh?! There can't be more than two players!"
+    end
   end
 end
