1889c7222971b2073997bcda580778e0f9b61ab3
katie <katiepb525@gmail.com>
2023-02-03 12:59:23 -0600

    add reader methods to player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,7 @@
 # Stores information about a player instance.
 class Player
+  attr_reader :name, :light_or_dark
+
   def initialize(name, light_or_dark = :light)
     @name = name
     @light_or_dark = light_or_dark
