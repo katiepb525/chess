048d16b654cfdf35c89ecda98410b267a692aa29
katie <katiepb525@gmail.com>
2023-02-04 18:48:14 -0600

    create rudimentary game handler class

---
+++ lib/game_handler.rb
@@ -0,0 +1,19 @@
+# Manages aspects concerning rounds, game rules, checkmate/stalemate conditions, saving and loading game. 
+# (Might be too many responsibilites, though.)
+class GameHandler
+  def initialize
+    @turn = 0
+    @player1 = Player.new('one')
+    @player2 = Player.new('two')
+    @board_handler = BoardHandler.new
+  end
+
+  def play_round
+    @turn += 1
+    if @turn.odd?
+      @player1
+    else
+      @player2
+    end
+  end
+end
