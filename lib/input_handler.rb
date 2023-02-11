f3292810036e8764ea84e58a9bd589ccabfa853b
katie <katiepb525@gmail.com>
2023-02-11 17:48:20 -0600

    unprivatize notation_to_coordinates

--- lib/input_handler.rb
+++ lib/input_handler.rb
@@ -14,28 +14,6 @@ class InputHandler
     @chosen_place = nil
   end
 
-  private
-
-  # Player can select a piece and move it in two inputs 'g4' 'd2'
-  # Player can select a piece and move in it one string 'g4d2'
-  def notation_is_valid?
-    @notation.length == 2 || @notation.length == 4
-    case @notation.length
-    when 2
-      FILES.include?(@notation[0])
-      RANKS.include?(@notation[1])
-      @notation[0..1] != @chosen_piece # should throw an error when triggered
-    when 4
-      FILES.include?(@notation[0])
-      RANKS.include?(@notation[1])
-      FILES.include?(@notation[2])
-      RANKS.include?(@notation[3])
-      @notation[0..1] != @notation[2..3] # should throw an error when triggered
-    else
-      false
-    end
-  end
-
   # Uses a modified form of algebraic notation to determine moves.
   def notation_to_coordinates
     return unless notation_is_valid?
@@ -54,6 +32,28 @@ class InputHandler
       coordinates[:x_coord] = file_to_coordinate(@notation[2])
       coordinates[:y_coord] = rank_to_coordinate(@notation[3])
       @chosen_place = coordinates.clone
+    end
+  end
+
+  private
+  
+  # Player can select a piece and move it in two inputs 'g4' 'd2'
+  # Player can select a piece and move in it one string 'g4d2'
+  def notation_is_valid?
+    @notation.length == 2 || @notation.length == 4
+    case @notation.length
+    when 2
+      FILES.include?(@notation[0])
+      RANKS.include?(@notation[1])
+      @notation[0..1] != @chosen_piece # should throw an error when triggered
+    when 4
+      FILES.include?(@notation[0])
+      RANKS.include?(@notation[1])
+      FILES.include?(@notation[2])
+      RANKS.include?(@notation[3])
+      @notation[0..1] != @notation[2..3] # should throw an error when triggered
+    else
+      false
     end
   end
 
