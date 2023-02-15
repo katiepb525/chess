8eba71792d84f6f0388674837e5505453874f3a6
katie <katiepb525@gmail.com>
2023-02-14 20:01:10 -0600

    (WIP they do not work) creating tests for board_handler_spec

--- spec/board_handler_spec.rb
+++ spec/board_handler_spec.rb
@@ -3,9 +3,9 @@
 require './lib/board_handler'
 
 describe BoardHandler do
-  context 'with an empty board' do
-    subject(:board_handler) { described_class.new }
+  subject(:board_handler) { described_class.new }
 
+  context 'with an empty board' do
     before do
       board_handler.set_up_board
     end
@@ -28,6 +28,44 @@ describe BoardHandler do
       expect(bottom_left_place.current_piece.color).to eq(:light)
       expect(bottom_right_place.current_piece.class.name).to eq('Rook')
       expect(bottom_right_place.current_piece.color).to eq(:light)
+    end
+  end
+
+  context 'with legal notation' do
+    let(:pawn) { double('Pawn')}
+    let(:place_of_piece_to_move) { board_handler.board.grid[6][3] }
+    let(:place_to_move_to) {board_handler.board.grid[5][3]}
+    before do
+      place_of_piece_to_move.current_piece = pawn
+      raw_input = 'd2d3'
+      board_handler.move_piece(raw_input)
+    end
+
+    it 'allows pawn movement within legal bounds' do
+      moved_piece = place_to_move_to.current_piece
+      expect moved_piece.to eq(pawn)
+    end
+
+    # it 'allows knight movement within legal bounds' do
+
+    # end
+
+    # it 'allows queen movement within legal bounds' do
+
+    # end
+
+    # it 'allows bishop movement within legal bounds' do
+    # end
+
+    # it 'allows king movement within legal bounds' do
+    # end
+
+    # it 'allows rook movement within legal bounds' do
+    # end
+  end
+
+  context 'with illegal notation' do
+    it 'throws an error message' do
     end
   end
 end
