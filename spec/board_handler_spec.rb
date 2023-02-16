a357a94f46dc76dc58c3cc92dbafeb7bbb9cca15
katie <katiepb525@gmail.com>
2023-02-15 18:58:30 -0600

    cleanup messiness caused by merging conflicts

--- spec/board_handler_spec.rb
+++ spec/board_handler_spec.rb
@@ -11,19 +11,19 @@ describe BoardHandler do
         board_handler.set_up_board
       end
 
-        it 'places dark rooks in correct spots' do
-          top_left_place = board_handler.board.grid[0][0]
-          top_right_place = board_handler.board.grid[0][7]
+      it 'places dark rooks in correct spots' do
+        top_left_place = board_handler.board.grid[0][0]
+        top_right_place = board_handler.board.grid[0][7]
 
-          expect(top_left_place.current_piece.class.name).to eq('Rook')
-          expect(top_left_place.current_piece.color).to eq(:dark)
-          expect(top_right_place.current_piece.class.name).to eq('Rook')
-          expect(top_right_place.current_piece.color).to eq(:dark)
-        end
+        expect(top_left_place.current_piece.class.name).to eq('Rook')
+        expect(top_left_place.current_piece.color).to eq(:dark)
+        expect(top_right_place.current_piece.class.name).to eq('Rook')
+        expect(top_right_place.current_piece.color).to eq(:dark)
+      end
 
-        it 'places light rooks in correct spots' do
-          bottom_left_place = board_handler.board.grid[7][0]
-          bottom_right_place = board_handler.board.grid[7][7]
+      it 'places light rooks in correct spots' do
+        bottom_left_place = board_handler.board.grid[7][0]
+        bottom_right_place = board_handler.board.grid[7][7]
 
         expect(bottom_left_place.current_piece.class.name).to eq('Rook')
         expect(bottom_left_place.current_piece.color).to eq(:light)
@@ -34,10 +34,11 @@ describe BoardHandler do
   end
 
   describe '#move_piece' do
-    let(:pawn) { double('Pawn')}
     context 'with legal notation' do
+      let(:pawn) { double('Pawn')}
       let(:place_of_piece_to_move) { board_handler.board.grid[6][3] }
       let(:place_to_move_to) {board_handler.board.grid[5][3]}
+
       before do
         place_of_piece_to_move.current_piece = pawn
         raw_input = 'd2d3'
@@ -62,23 +63,4 @@ describe BoardHandler do
 
   # end
 
-  # it 'allows queen movement within legal bounds' do
-
-  # end
-
-  # it 'allows bishop movement within legal bounds' do
-  # end
-
-  # it 'allows king movement within legal bounds' do
-  # end
-
-  # it 'allows rook movement within legal bounds' do
-  # end
-end
-
-    context 'with illegal notation' do
-      it 'throws an error message' do
-      end
-    end
-  end
 end
