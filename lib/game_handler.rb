1a418ed6b0de306c3e685344a41e579daa8ca0fe
Katherine Powell Bianco <katiepb525@gmail.com>
2023-02-26 21:28:54 -0600

    refactoring gamehandler to have less dependencies (WIP)

--- lib/game_handler.rb
+++ lib/game_handler.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require './lib/board_handler'
+require './lib/input_handler'
 require './lib/display'
 require './lib/player'
 require './lib/movement'
@@ -15,7 +16,7 @@ class GameHandler
     @player2 = Player.new(2)
     @board_handler = BoardHandler.new
     @input_handler = InputHandler.new
-    @movement = nil
+    @movement = Movement.new(@board_handler.board)
   end
 
   def start_game
@@ -47,41 +48,15 @@ class GameHandler
     play_game
   end
 
-  def handle_player_input(current_player)
-    current_player.raw_input = gets.chomp
-    notation_valid = @input_handler.notation_is_valid?(current_player.raw_input)
-
-    until notation_valid
-      if notation_valid == false
-        puts display_bad_input
-      end
-      current_player.raw_input = gets.chomp
-      notation_valid = @input_handler.notation_is_valid?(current_player.raw_input)
-    end
-  end
-
-  def handle_player_movement(current_player) # Kind of messy?
-    @movement = Movement.new(current_player.raw_input, @board_handler.board, @input_handler)
-    case @movement.ok_to_move_to?
-    when true
-      @movement.move_piece
-    when false
-      until @movement.ok_to_move_to?
-        puts "That move is illegal. Please enter a legal move."
-        handle_player_input(current_player)
-        @movement = Movement.new(current_player.raw_input, @board_handler.board, @input_handler)
-      end
-      @movement.move_piece
-    end
-  end
 
   def play_game
     until game_over? # will loop indefinetely! wip
       current_player = play_round
       display_board(@board_handler)
       puts ask_input(current_player)
-      handle_player_input(current_player)
-      handle_player_movement(current_player)
+      @input_handler.handle_player_input
+      @input_handler.translate_coordinates(@board_handler.board)
+      @movement.handle_player_movement(@input_handler)
       display_board(@board_handler)
     end
     # game_over_msg(current_player)
